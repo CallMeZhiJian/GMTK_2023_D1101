fccd8f48ce8dc84c61a52230a8a331f5b98314b3
CallMeZhiJian <zjiandev@gmail.com>
2023-07-09 01:40:51 +0800

    Proof of Concept

--- Assets/Scripts/DragNShoot.cs
+++ Assets/Scripts/DragNShoot.cs
@@ -67,7 +67,6 @@ public class DragNShoot : MonoBehaviour
     {
         if (collision.CompareTag("Enemy"))
         {
-            Debug.Log("SlowDown");
             timeManager.DoSlowMo();
             Destroy(collision.gameObject);
         }
