b454274739f864215c1e214e525c70d1fc66d3bc
CallMeZhiJian <zjiandev@gmail.com>
2023-07-14 16:50:52 +0800

    Tutorial aniamtion done

--- Assets/TutorialMenu.cs
+++ Assets/TutorialMenu.cs
@@ -6,10 +6,30 @@ using UnityEngine.SceneManagement;
 public class TutorialMenu : MonoBehaviour
 {
     [SerializeField] private AudioClip clickButtonSFX;
-    public void nextGame()
+    [SerializeField] private Animator tutorialAnim;
+    
+    private void Start()
     {
-        SceneManager.LoadScene("GameScene");
-        SoundManager.instance.PlaySound(clickButtonSFX);
+        StartCoroutine(startScene());
     }
 
+    public void nextGame()
+    {
+        StartCoroutine(endScene());
+        SoundManager.instance.PlaySound(clickButtonSFX);
+        
+    }
+
+    IEnumerator startScene()
+    {
+        yield return new WaitForSeconds(0.5f);
+        tutorialAnim.SetBool("isOpened", true);
+    }
+
+    IEnumerator endScene()
+    {
+        tutorialAnim.SetBool("isOpened", false);
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("GameScene");
+    }
 }
